b7a98c1fafcf84438c6eb5e31140b95a572d4c35
cesadom <37183706+cesadom@users.noreply.github.com>
2018-12-05 07:20:16 +0100

    easy API

--- analyzer/views.py
+++ analyzer/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 
 from django.conf import settings
 import solaredge
+import requests
+
 
 APIKEY = getattr(settings, "solarEdgeAPIKey", None)
 APIID = getattr(settings, "solarEdgeID", None)
@@ -11,6 +13,13 @@ APIID = getattr(settings, "solarEdgeID", None)
 
 
 def home(request):
+    api_address='http://api.openweathermap.org/data/2.5/weather?appid=0c42f7f6b53b244c78a418f4f181282a&q='
+    url = api_address + city
+    json_data = requests.get(url).json()
+    format_add = json_data['base']
+    print(format_add)
+
+
     # http = urllib3.PoolManager()
     # req = http.request('GET', 'https://monitoringapi.solaredge.com/site/' + solarEdgeID + '/overview?api_key=' + solarEdgeAPIKey)
     # print(req)
