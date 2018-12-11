deb42afcde3a5be7fd59706ef5c27267d5ec3f39
Domenico Cesare <domenico.cesare@gmail.com>
2018-12-11 18:04:50 +0100

    fixed missing login_required

--- analyzer/views.py
+++ analyzer/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 from django.conf import settings
 import solaredge
